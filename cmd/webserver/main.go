ff1c98a55dac2e721aac8940aa4871097e4737b4
caespinosam <cesar917@gmail.com>
2017-03-28 17:03:39 +0200

    add CORS support on the webserver

--- cmd/webserver/main.go
+++ cmd/webserver/main.go
@@ -9,6 +9,7 @@ import (
 	"github.com/tendermint/clearchain/client"
 	"github.com/tendermint/clearchain/types"
 	"github.com/tendermint/go-crypto"
+	"github.com/gorilla/mux"
 )
 
 var serverAddress = "127.0.0.1:46657"
@@ -23,6 +24,26 @@ const (
 	PrivateKey       = "privateKey"
 	Help             = "help"
 )
+
+
+type WebServer struct {
+    r *mux.Router
+}
+
+func (s *WebServer) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
+    if origin := req.Header.Get("Origin"); origin != "" {
+        rw.Header().Set("Access-Control-Allow-Origin", origin)
+        rw.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+        rw.Header().Set("Access-Control-Allow-Headers",
+            "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+    }
+    // Stop here if its Preflighted OPTIONS request
+    if req.Method == "OPTIONS" {
+        return
+    }
+    // Lets Gorilla work
+    s.r.ServeHTTP(rw, req)
+}
 
 func main() {
 	handleCommandLine()
@@ -74,9 +95,13 @@ func flagHandler(currentFlag *flag.Flag) {
 	}
 }
 
+
 func startWebserver() {
-	http.HandleFunc("/view/", viewHandler)
-	http.HandleFunc("/", defaultHandler)
+	
+	r := mux.NewRouter()
+    r.HandleFunc("/view/", viewHandler)
+	
+	http.Handle("/", &WebServer{r})
 
 	http.ListenAndServe(":8080", nil)
 }
