2b75c942f06c7d36ef166d090deb83ee6f63b25c
Alessio Treglia <alessio@debian.org>
2018-03-10 09:46:22 +0000

    Add -version to clearchain cli

--- cmd/clearchain/main.go
+++ cmd/clearchain/main.go
@@ -4,7 +4,9 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"path"
 
+	"github.com/tendermint/clearchain"
 	"github.com/tendermint/clearchain/app"
 
 	abci "github.com/tendermint/abci/types"
@@ -16,10 +18,24 @@ import (
 // Entry point of the Go app
 
 func main() {
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "Usage: %s [OPTION]...\n", path.Base(os.Args[0]))
+		fmt.Fprintf(os.Stderr, `
+ -address Listen address (default "0.0.0.0:46658")
+ -genesis Genesis file, if any
+ -version output version information and exit
+`)
+	}
 	// Read application flags
 	addrPtr := flag.String("address", "0.0.0.0:46658", "Listen address")
 	genFilePath := flag.String("genesis", "", "Genesis file, if any")
+	version := flag.Bool("version", false, "output version information and exit")
 	flag.Parse()
+
+	if *version {
+		fmt.Println(clearchain.Version)
+		os.Exit(0)
+	}
 
 	// Create Clearchain app. It creates a /data folder
 	logger := log.NewTMLogger(log.NewSyncWriter(os.Stdout)).With("module", "main")
