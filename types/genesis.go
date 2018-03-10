1bf46d0d935c738445b08276d752f9cf6c68d326
Alessio Treglia <alessio@debian.org>
2018-03-10 10:14:48 +0000

    Quick cleanup

--- types/genesis.go
+++ types/genesis.go
@@ -15,20 +15,10 @@ type GenesisState struct {
 type GenesisAccount struct {
 	PubKeyHexa string `json:"public_key"`
 	EntityName string `json:"entity_name"`
-
-	/* future support
-	EntityType string `json:"entity_type"`
-	Creator 	crypto.Address 	`json:"creator_address"`
-	Coins 		sdk.Coins    	`json:"coins"`
-	IsActive    bool 			`json:"is_active"`
-	IsAdmin     bool 			`json:"is_admin"`
-	AccountType string 			`json:"account_type"`
-	*/
 }
 
 // ToClearingHouseAdmin converts  a GenesisAccount into an AppAccount (a Clearing House admin user)
 func (ga *GenesisAccount) ToClearingHouseAdmin() (acc *AppAccount, err error) {
-
 	// Done manually since JSON Unmarshalling does not create a PubKey from a hexa value
 	pubBytes, err := hex.DecodeString(ga.PubKeyHexa)
 	if err != nil {
