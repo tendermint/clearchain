f243a9952f8f29b34c3b5c1cf594543861f8367f
Alessio Treglia <alessio@debian.org>
2018-04-08 19:22:44 +0100

    Export public key to JSON

--- commands/exportPub.go
+++ commands/exportPub.go
@@ -1,14 +1,10 @@
 package commands
 
 import (
-	"encoding/hex"
 	"fmt"
-
-	"github.com/spf13/viper"
 
 	"github.com/cosmos/cosmos-sdk/client/keys"
 	wire "github.com/cosmos/cosmos-sdk/wire"
-	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
 )
 
@@ -20,43 +16,25 @@ func GetExportPubCmd(cdc *wire.Codec) *cobra.Command {
 	cmd := &cobra.Command{
 		Use:  "export-pub",
 		RunE: exportPubCmd,
+		Args: cobra.ExactArgs(1),
 	}
 	cmd.Flags().String(flagFormat, "", "Alternative output format (armor|json)")
 	return cmd
 }
 
 func exportPubCmd(cmd *cobra.Command, args []string) error {
-	if len(args) < 1 {
-		return fmt.Errorf("insufficient arguments")
-	}
-	if len(args) > 1 {
-		return fmt.Errorf("too many arguments: %v", args)
-	}
-	name := args[0]
 	keybase, err := keys.GetKeyBase()
 	if err != nil {
 		return err
 	}
-	if viper.GetString(flagFormat) == "armor" {
-		out, err := keybase.Export(name)
-		if err != nil {
-			return err
-		}
-		fmt.Println(out)
-		return nil
-	}
-	info, err := keybase.Get(name)
+	key, err := keybase.Get(args[0])
 	if err != nil {
-		return errors.Errorf("No key for: %s", name)
+		return err
 	}
-	if viper.GetString(flagFormat) == "json" {
-		out, err := info.PubKey.MarshalJSON()
-		if err != nil {
-			return err
-		}
-		fmt.Println(string(out))
-		return nil
+	bz, err := key.PubKey.MarshalJSON()
+	if err != nil {
+		return err
 	}
-	fmt.Println(hex.EncodeToString(info.PubKey.Bytes()))
+	fmt.Println(string(bz))
 	return nil
 }
