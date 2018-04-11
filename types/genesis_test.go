482c33b50e8b77ed1a9de2900dcaf4b54947b089
Alessio Treglia <alessio@debian.org>
2018-04-11 21:02:11 +0100

    Remove PubKeyFromBytes, no longer necessary

--- types/genesis_test.go
+++ types/genesis_test.go
@@ -6,22 +6,19 @@ import (
 
 	"github.com/cosmos/cosmos-sdk/wire"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	crypto "github.com/tendermint/go-crypto"
 )
 
 // ToClearingHouseAdmin verifies that a GenesisAccount is converted correctly into an AppAccount (a Clearing House admin user)
 func Test_ToClearingHouseAdmin(t *testing.T) {
+	var publicKey1, publicKey2, publicKey3 crypto.PubKey
 	cdc := MakeCodec()
 	wire.RegisterCrypto(cdc)
 
-	pubBytes, _ := hex.DecodeString("01328eaf59335aa6724f253ca8f1620b249bb83e665d7e5134e9bf92079b2549df3572f874")
-	publicKey1, _ := crypto.PubKeyFromBytes(pubBytes)
-
-	pubBytes, _ = hex.DecodeString("01328eaf59335aa6724f253ca8f1620b249bb83e665d7e5134e9bf92079b2549df3572f875")
-	publicKey2, _ := crypto.PubKeyFromBytes(pubBytes)
-
-	pubBytes, _ = hex.DecodeString("01328eaf59335aa6724f253ca8f1620b249bb83e665d7e5134e9bf92079b2549df3572f876")
-	publicKey3, _ := crypto.PubKeyFromBytes(pubBytes)
+	require.Nil(t, publicKey1.UnmarshalJSON([]byte(`{"type":"ed25519","data":"328eaf59335aa6724f253ca8f1620b249bb83e665d7e5134e9bf92079b2549df3572f874"}`)))
+	require.Nil(t, publicKey2.UnmarshalJSON([]byte(`{"type":"ed25519","data":"328eaf59335aa6724f253ca8f1620b249bb83e665d7e5134e9bf92079b2549df3572f875"}`)))
+	require.Nil(t, publicKey3.UnmarshalJSON([]byte(`{"type":"ed25519","data":"328eaf59335aa6724f253ca8f1620b249bb83e665d7e5134e9bf92079b2549df3572f876"}`)))
 
 	adminCreated1 := NewAdminUser(publicKey1, nil, "ClearChain", "ch")
 	adminCreated2 := NewAdminUser(publicKey2, nil, "ClearingHouse", "ch")
@@ -65,27 +62,6 @@ func Test_ToClearingHouseAdmin(t *testing.T) {
 			assert.True(t, adminUser.Active)
 			assert.Equal(t, tt.expectedAccount.Admin, adminUser.Admin)
 			assert.True(t, adminUser.Admin)
-		})
-	}
-}
-
-func TestPubKeyFromHexString(t *testing.T) {
-	type args struct {
-		s string
-	}
-	tests := []struct {
-		name    string
-		args    args
-		wantErr bool
-	}{
-		{"good key", args{"01328eaf5937458f6c39c54ee3624137cabe7af88226454fd30180c8da6c711ad6de7f6053"}, false},
-		{"bad key", args{"0124137cabe7af88226454fd30180c8da6c711ad6de7f6053"}, true},
-		{"nil", args{""}, true},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			_, err := PubKeyFromHexString(tt.args.s)
-			assert.Equal(t, (err != nil), tt.wantErr)
 		})
 	}
 }
